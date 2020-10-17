8991143054322077781c7ad40d05d1b050d0ef09
medapalli <medapalli@yahoo.com>
2020-10-17 22:00:18 +0530

    test file changes
    test file changes

--- Mid_Term_Exam/test_exchange.py
+++ Mid_Term_Exam/test_exchange.py
@@ -1,8 +1,8 @@
 from MidTerm_ExchangeRate_Prog6 import exchange_lookup
 
 def ExchangeRate_lookup():
-    assert zipcode_lookup(url,100, "INR") == 7335.450899999999
-    assert zipcode_lookup(url,200, "CAD") == 132.0121
-    assert zipcode_lookup(url,"zzz", "INR") == "Invalid input"
-    assert zipcode_lookup(url,USD, "zzz") == "Invalid input"
+    assert zipcode_lookup("https://api.exchangerate-api.com/v4/latest/USD",100, "INR") == 7335.450899999999
+    assert zipcode_lookup("https://api.exchangerate-api.com/v4/latest/USD",200, "CAD") == 132.0121
+    assert zipcode_lookup("https://api.exchangerate-api.com/v4/latest/USD","zzz", "INR") == "Invalid input"
+    assert zipcode_lookup("https://api.exchangerate-api.com/v4/latest/USD",USD, "zzz") == "Invalid input"
     
